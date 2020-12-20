b7defcfc1390a39bb78a76b64aabce110baf884e
Pooja N G <43085809+PoojaNandihalliGirish99@users.noreply.github.com>
2020-12-20 14:10:44 +0530

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import React, { useState } from "react";
 import "./styles.css";
-var emojiList = ["x", "y", "z"];
 
 var emojiDictionary = {
   "♈": "Aries",
   "♉": "Taurus",
   "♊": "Gemini",
   "♋": "Cancer",
-  "♌": "Leo"
-  // "♍": "Virgo",
-  // "♎": "Libra",
-  // "♏": "Scorpio",
-  // "♐": "Sagittarius",
-  // "♑": "Capricorn",
-  // "♒": "Aquarius",
-  // "♓": "Pisces",
-  // "⛎": "Ophiuchus"
+  "♌": "Leo",
+  "♍": "Virgo",
+  "♎": "Libra",
+  "♏": "Scorpio",
+  "♐": "Sagittarius",
+  "♑": "Capricorn",
+  "♒": "Aquarius",
+  "♓": "Pisces",
+  "⛎": "Ophiuchus"
 };
 var emojiListWeknow = Object.keys(emojiDictionary);
 
 export default function App() {
-  const [likeCounter, setLikeCounter] = useState(0);
-  // const [userInput, setUserInput] = useState(" ");
-  const [meaning, setMeaning] = useState(" ");
+  // const [likeCounter, setLikeCounter] = useState(0);
 
+  const [meaning, setMeaning] = useState(" ");
+  var label = "Type the zodiac emoji.";
   function inputEventHandler(event) {
-    // console.log(event.target.value);
-    // setUserInput(event.target.value);
     var userInput = event.target.value;
     var meaning = emojiDictionary[userInput];
     if (meaning === undefined) {
@@ -34,47 +31,56 @@ export default function App() {
     }
     setMeaning(meaning);
   }
-
-  function likeClickHandler() {
-    var updatedLikeCounter = likeCounter + 1;
-    setLikeCounter(updatedLikeCounter);
-  }
   function emojiClickHandler(emoji) {
-    // console.log(emoji);
     var meaning = emojiDictionary[emoji];
     setMeaning(meaning);
   }
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <input onChange={inputEventHandler}></input>
+      <h1>Welcome</h1>
+      <h4>Know the differnt zodiac signs and their name.</h4>
+      <input
+        onChange={inputEventHandler}
+        placeholder={label}
+        style={{
+          padding: "0.5rem",
+          margin: "0.5rem",
+          width: "40%",
+          backgroundColor: "blanchedalmond"
+        }}
+      ></input>
       <br />
-      <div>
-        <ul>
-          {emojiList.map((item) => {
-            console.log("item");
-            return <li key={item}>{item}</li>;
-          })}
-        </ul>
-      </div>
-      <h3>Emoji's we know</h3>
+      <h4>Tap on the emoji below.</h4>
       {emojiListWeknow.map(function (emoji) {
         return (
-          <span
+          <button
             onClick={() => emojiClickHandler(emoji)}
-            style={{ fontSize: "1.2rem", padding: "0.5rem", cursor: "pointer" }}
+            style={{
+              fontSize: "1.2rem",
+              margin: "0.5rem 0.5rem",
+              cursor: "pointer",
+              listStyle: "none",
+              borderRadius: "0rem",
+              padding: "auto",
+              backgroundColor: "darkkhaki"
+            }}
             key={emoji}
           >
             {emoji}
-          </span>
+          </button>
         );
       })}
 
       <br />
-      <div>{meaning}</div>
-      <button onClick={likeClickHandler}>Like me</button>
-      {likeCounter}
+      <div
+        style={{
+          width: "50%",
+          margin: "auto"
+        }}
+      >
+        <h4 key={meaning}>The Zodiac name is - {meaning}</h4>
+      </div>
     </div>
   );
 }
